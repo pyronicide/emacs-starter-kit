2bc6d5b024107c19776250fe54e94214a670506d
Thomas Rampelberg <trampelb@grue.612howard.bit>
2010-02-16 09:50:46 -0800

    Stupid reappearing scroll bar, i don't want you

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 ;; You really don't need these; trust me.
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; Load path etc.
 
